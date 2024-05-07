70370b7cfc1de269ba9138129c0f252f8c5956f7
Vladimirkasperovich <vova.kasper.boy@gmail.com>
2024-05-07 17:06:38 +0300

    Added formik form

--- src/components/TransitionsModal.tsx
+++ src/components/TransitionsModal.tsx
@@ -1,61 +1,72 @@
-import * as React from 'react';
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { TextField } from '@mui/material';
-import { InputWrapper } from './InputWrapper';
-import { ButtonWrapper } from './ButtonWrapper';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from 'store/store';
-import { cityAction } from 'reducers/city.reducer';
-import { log } from 'console';
+import * as React from "react";
+import Backdrop from "@mui/material/Backdrop";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Fade from "@mui/material/Fade";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { TextField } from "@mui/material";
+import { InputWrapper } from "./InputWrapper";
+import { ButtonWrapper } from "./ButtonWrapper";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "store/store";
+import { cityAction } from "reducers/city.reducer";
+import { log } from "console";
+import { useFormik } from "formik";
 
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
   height: 220,
-  bgcolor: 'background.paper',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'space-around',
+  bgcolor: "background.paper",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-around",
   boxShadow: 24,
   p: 4,
 };
 
+export type TransitionsModalPropsType = {
+  toggleTransitionsModal: () => void;
+  isOpenTransitionsModal: boolean;
+};
 
-export type  TransitionsModalPropsType = {
-  toggleTransitionsModal: () => void
-  isOpenTransitionsModal: boolean
-}
-
-
-export const TransitionsModal = ({isOpenTransitionsModal, toggleTransitionsModal}: TransitionsModalPropsType) => {
+export const TransitionsModal = ({
+  isOpenTransitionsModal,
+  toggleTransitionsModal,
+}: TransitionsModalPropsType) => {
   const dispatch = useDispatch<AppDispatch>();
   const handleClose = () => toggleTransitionsModal();
-  const [city, setCity] = React.useState('');
-  const [countryCode, setCountryCode] = React.useState('');
-
+  const [city, setCity] = React.useState("");
+  const [countryCode, setCountryCode] = React.useState("");
 
   const addCurrentCity = () => {
-    // dispatch(cityAction({}))
-  }
+    dispatch(cityAction({code: countryCode, label: city}))
+  };
 
   const setCurrentCity = (value: string) => {
-     setCity(value)
-  }
+    setCity(value);
+  };
 
   const setCurrentCountryCode = (value: string) => {
-      setCountryCode(value)
-  }
-   
-  
+    setCountryCode(value);
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      code: '',
+      label: ''
+    },
+    onSubmit: value => {
+
+    }
+  })
+
+  console.log(city);
   return (
     <div>
       <Modal
@@ -72,13 +83,25 @@ export const TransitionsModal = ({isOpenTransitionsModal, toggleTransitionsModal
         }}
       >
         <Fade in={isOpenTransitionsModal}>
-          <Box sx={style}> 
-               <InputWrapper label='Your city' variant='outlined' onInputChange={setCurrentCity}/>
-               <InputWrapper label='Country code' variant='outlined' onInputChange={setCurrentCountryCode}/>
-               <ButtonWrapper text='ok' variant='contained' clickHandler={addCurrentCity}/>
+          <Box sx={style}>
+            <InputWrapper
+              label="Your city"
+              variant="outlined"
+              onInputChange={setCurrentCity}
+            />
+            <InputWrapper
+              label="Country code"
+              variant="outlined"
+              onInputChange={setCurrentCountryCode}
+            />
+            <ButtonWrapper
+              text="ok"
+              variant="contained"
+              clickHandler={addCurrentCity}
+            />
           </Box>
         </Fade>
       </Modal>
     </div>
   );
-}
+};
